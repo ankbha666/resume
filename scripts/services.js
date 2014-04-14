a7ce62b20415a615f701e634bf2a6a46124b5372
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-04-13 19:07:14 -0700

    Implement simple CLI which loads resume from linkedin

--- scripts/services.js
+++ scripts/services.js
@@ -1,15 +1,30 @@
 angular.module('services')
   .service('EngineerInfoService', ['$q', '$http', function($q, $http) {
 
-    function getLinkedInInfo(callback) {
-      $http({method: 'GET', url: '  scripts/linkedin.json'})
+    function load(path, callback) {
+      $http({method: 'GET', url: path})
         .success(callback);
     }
 
     this.info = function() {
       var defer = $q.defer();
-      getLinkedInInfo(function(data) {
-        defer.resolve(data);
+      load('scripts/linkedin.json', function(info) {
+        load('scripts/extra_info.json', function(extraInfo) {
+          var nameToProject = _.object(_.map(info.projects, function(item) {
+            return [item.name, item];
+          }));
+          var companyToProjects = _.object(_.map(extraInfo.projectMappings, function(mapping) {
+            return [mapping.company, _.map(mapping.projects, function(name) {
+              return nameToProject[name];
+            })];
+          }));
+          _.each(info.careerHistory, function(item) {
+            item.projects = companyToProjects[item.company.name];
+          });
+          info.photoImageUrl = extraInfo.photoImageUrl;
+
+          defer.resolve(info);
+        })
       });
       return defer.promise;
     }
