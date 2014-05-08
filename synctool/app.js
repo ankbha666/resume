3bdd2c46f7e961ce1023fa112ddd2409f468ba13
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-05-07 23:37:33 -0700

    Rewrite export from linkedin using nodejs

---
+++ synctool/app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var linkedIn = require('./linkedin');
+var _ = require('underscore');
+
+var session = {};
+
+var servicesDefinitions = [{
+  name : 'linkedin',
+  service: linkedIn.service,
+  defaultActionName: 'exportResume'
+}];
+
+function getServiceActionUrl(definition, action) {
+  return '/' + definition.name + '/' + action;
+}
+
+function getServiceSession(definition) {
+  var result = session[definition.name];
+  if (!result) {
+    result = {};
+    session[definition.name] = result;
+  }
+  return result;
+}
+
+_.each(servicesDefinitions, function(definition) {
+  var service = definition.service;
+  service.init({
+    verifyUrl: 'http://localhost:8080/' + definition.name + '/verify'
+  });
+  app.get('/' + definition.name + '/verify', function(req, res) {
+    var serviceSession = getServiceSession(definition);
+    if (_.has(serviceSession, 'requestToken')) {
+      service.verifyOAuthToken(serviceSession.requestToken, req.query.oauth_verifier, function(token) {
+        serviceSession.token = token;
+        res.redirect(getServiceActionUrl(
+          definition, serviceSession.requestedActionName || definition.defaultActionName));
+      });
+    } else {
+      res.send('Request token not found!');
+    }
+  });
+
+  _.each(definition.service.actions, function(action) {
+    app.get(getServiceActionUrl(definition, action.name), function(req, res) {
+      var serviceSession = getServiceSession(definition);
+      if (!_.has(serviceSession, 'token')) {
+        serviceSession.requestedActionName = action.name;
+        service.requestOAuthToken(function(redirectUrl, requestToken) {
+          serviceSession.requestToken = requestToken;
+          res.redirect(redirectUrl);
+        });
+      } else {
+        action.execute(serviceSession.token, function(result) {
+          res.send(result);
+        });
+      }
+    });
+  })
+});
+
+app.listen(8080);
---
+++ synctool/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  'linkedin': {
+    "apiKey": '7541dqbk91i989',
+    'apiSecret': 'd0mTWCLJ7CrApNfn'
+  }
+}
