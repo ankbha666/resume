9fdb26ab1d26418a18ad076c67ad7403f208d25d
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-05-08 23:21:23 -0700

    Configure build task for grunt (no js minification yet)

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,62 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    app :{
+      path: 'webapp',
+      dist: 'dist'
+    },
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= app.path %>',
+            src: './**/*',
+            dest: '<%= app.dist %>'
+          }
+        ]
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeCommentsFromCDATA: true,
+          removeOptionalTags: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= app.path %>',
+            src: ['index.html'],
+            dest: '<%= app.dist %>'
+          }
+        ]
+      }
+    },
+    clean: {
+      dist: {
+        files: [
+          {
+            src: [ '<%= app.dist %>' ]
+          }
+        ]
+      }
+    },
     bowerInstall: {
       target: {
         src: [
-          'webapp/index.html'
+          '<%= app.path %>/index.html'
         ]
       }
     }
   });
-  grunt.registerTask('default', ['bowerInstall']);
+  grunt.registerTask('build',[
+    'clean:dist',
+    'bowerInstall',
+    'copy:dist',
+    'htmlmin'
+  ]);
+  grunt.registerTask('default', ['build']);
 };
