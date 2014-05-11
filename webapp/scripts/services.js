6f1663432d5aac647b363d74c30f276e5f4ca05f
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-05-11 11:22:58 -0700

    Store resume in XML instead of JSON

--- webapp/scripts/services.js
+++ webapp/scripts/services.js
@@ -1,31 +1,28 @@
 angular.module('services')
-  .service('EngineerInfoService', ['$q', '$http', function($q, $http) {
+  .service('EngineerInfoService', ['$q', '$http', function ($q, $http) {
 
     function load(path, callback) {
       $http({method: 'GET', url: path})
         .success(callback);
     }
 
-    this.info = function() {
+    this.info = function () {
       var defer = $q.defer();
-      load('data/linkedin.json', function(info) {
-        // load information which is not available in linked in.
-        load('data/extra_info.json', function(extraInfo) {
-          var nameToProject = _.object(_.map(info.projects, function(item) {
-            return [item.name, item];
-          }));
-          var companyToProjects = _.object(_.map(extraInfo.projectMappings, function(mapping) {
-            return [mapping.company, _.map(mapping.projects, function(name) {
-              return nameToProject[name];
-            })];
-          }));
-          _.each(info.careerHistory, function(item) {
-            item.projects = companyToProjects[item.company.name];
-          });
-          info.photoImageUrl = extraInfo.photoImageUrl;
-
-          defer.resolve(info);
-        })
+      load('data/resume.xml', function (xml) {
+        function processArrays(item) {
+          for (var name in item) {
+            var val = item[name];
+            if (_.isObject(val)) {
+              if (val._array) {
+                item[name] = _.isArray(val.item) ? val.item : [val.item];
+              }
+              processArrays(val);
+            }
+          }
+          return item;
+        }
+        var info = processArrays(new X2JS().xml_str2json(xml).data);
+        defer.resolve(info);
       });
       return defer.promise;
     }
