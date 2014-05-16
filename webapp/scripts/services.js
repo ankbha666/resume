f507caeed6353363d1c5db6e96c3f05a99a3d681
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-05-16 16:58:15 -0700

    Add a little of css. Update skills section.

--- webapp/scripts/services.js
+++ webapp/scripts/services.js
@@ -9,19 +9,25 @@ angular.module('services')
     this.info = function () {
       var defer = $q.defer();
       load('data/resume.xml', function (xml) {
-        function processArrays(item) {
+        function process(item) {
           for (var name in item) {
             var val = item[name];
+            // Get rid of attribute prefixed. Implemented here due to X2JS requires some prefix.
+            if (name.indexOf('_') == 0) {
+              item[name.slice(1)] = val;
+              delete item[name];
+            }
             if (_.isObject(val)) {
+              // Transform xml array into real json arrays.
               if (val._array) {
                 item[name] = _.isArray(val.item) ? val.item : [val.item];
               }
-              processArrays(val);
+              process(val);
             }
           }
           return item;
         }
-        var info = processArrays(new X2JS().xml_str2json(xml).data);
+        var info = process(new X2JS().xml_str2json(xml).data);
         defer.resolve(info);
       });
       return defer.promise;
