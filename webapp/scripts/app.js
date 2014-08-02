6ec07516d180fcc098ab4934301bc9a9a617b4e7
Alexander Matyushentsev <matyushentsev@matyushentsev-macbookpro.roam.corp.google.com>
2014-08-01 18:42:16 -0700

    Remove github activity page and get rid of routing

--- webapp/scripts/app.js
+++ webapp/scripts/app.js
@@ -1,17 +1,6 @@
 angular.module('controllers', []);
 angular.module('services', []);
 
-var webApp = angular.module('webApp', [
+angular.module('webApp', [
   'services',
-  'controllers',
-  'ngRoute']);
-
-webApp.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'views/resume.html',
-    controller: 'ResumeCtrl'
-  }).when('/github', {
-    templateUrl: 'views/github.html',
-    controller: 'GitHubCtrl'
-  })
-}]);
+  'controllers']);
